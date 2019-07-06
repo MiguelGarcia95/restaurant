4185e1171fd0dcbcadb433b93e90535d339e96e1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-05 23:21:53 -0500

    add gsap and scrollmagic

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 class App extends React.Component {
   componentDidMount() {
+    // window.addEventListener('load', () => {
+    //   document.getElementById('loading').classList.add('loaded');
+    // })
+
     var tl = new window.TimelineMax();
     const controller = new window.ScrollMagic.Controller();
 
-    tl.from('#test', .5, {opacity: 0, left: "-100vw"});
+    tl.from('#test', .5, {opacity: 0, left: "-100%"});
 
     const scene = new window.ScrollMagic.Scene({
       triggerElement: "#test",
@@ -18,6 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
+        {/* <section id='loading'>loading</section> */}
         <section className='welcome'>
 
         </section>
@@ -25,7 +30,7 @@ class App extends React.Component {
 
         </section>
         <section id='test'>
-
+          <h1>This slides in!</h1>
         </section>
       </div>
     );
