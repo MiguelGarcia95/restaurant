0840bbeaface333710e77b82eb91b85b3301a316
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-05 23:10:18 -0500

    add gsap and scrollmagic

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <section className="App">
-    </section>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    var tl = new window.TimelineMax();
+    const controller = new window.ScrollMagic.Controller();
+
+    tl.from('.welcome', .5, {opacity: 0});
+
+    const scene = new window.ScrollMagic.Scene({
+      triggerElement: ".welcome",
+    })
+    .setTween(tl)
+    .addTo(controller);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <section className='welcome'>
+
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
