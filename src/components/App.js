e8ff47bd2db219c3b66ec31acdfe8b3f1503fcb3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-08 18:02:55 -0500

    display contact slide

--- src/components/App.js
+++ src/components/App.js
@@ -96,7 +96,7 @@ class App extends React.Component {
           <section className='image_side'>
           </section>
         </section>
-        
+
         <section className='page_section menu'>
           <section className='menu_image_side'>
             <section className='cube shaded'></section>
@@ -120,7 +120,20 @@ class App extends React.Component {
             <section className='cube'></section>
           </section>
           <section className='menu_side'>
-            <h1>menu</h1>
+            <section className='menu_header'>
+              <h1>Our Menu</h1>
+              <p>Whether you're in for breakfast, a business lunch or a family dinner, we promise you'll find something to love.</p>
+            </section>
+            <section className='menu_items'>
+              <section className='menu_item'>
+                <section className='item_image'>
+                  <p className='item_price'>$9.99</p>
+                </section>
+                <section className='item_description'>
+                  <h2 className='item_title'>Straberry Vodka chocolate Cake</h2>
+                </section>
+              </section>
+            </section>
           </section>
         </section>
 
