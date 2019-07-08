3987571a36e2b2a38b385bec3deb3b8e142d65ef
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-07 23:41:04 -0500

    style home content side

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './styles.css';
 
 function Navbar({setCurrentSlide, currentSlide}) {
+  const activeSlide = slide => slide === currentSlide ? true : false; 
   return (
     <section className='navbar'>
-      <span onClick={() => setCurrentSlide('welcome')} ><p>Welcome</p></span>
-      <span onClick={() => setCurrentSlide('about')} ><p>About</p></span>
-      <span onClick={() => setCurrentSlide('menu')} ><p>Menu</p></span>
-      <span onClick={() => setCurrentSlide('contact')} ><p>Contact</p></span>
+      <span className={activeSlide('welcome') ? 'active' : ''} onClick={() => setCurrentSlide('welcome')} ><p>Welcome</p></span>
+      <span className={activeSlide('about') ? 'active' : ''} onClick={() => setCurrentSlide('about')} ><p>About</p></span>
+      <span className={activeSlide('menu') ? 'active' : ''} onClick={() => setCurrentSlide('menu')} ><p>Menu</p></span>
+      <span className={activeSlide('contact') ? 'active' : ''} onClick={() => setCurrentSlide('contact')} ><p>Contact</p></span>
     </section>
   );
 }
