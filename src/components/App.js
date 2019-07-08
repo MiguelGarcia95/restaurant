42e07f7b1cbf64418d3e03941160f9276641acb6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-07 23:18:46 -0500

    style home content side

--- src/components/App.js
+++ src/components/App.js
@@ -90,7 +90,7 @@ class App extends React.Component {
           <section className='content_side'>
             <h1>Hungry?</h1>
             <h1>Get a bite</h1>
-            <section className='content-button'>
+            <section className='content-button' onClick={() => this.setCurrentSlide('menu')}>
               <p>Check our Menu Out</p>
             </section>
           </section>
