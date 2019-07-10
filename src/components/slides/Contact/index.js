298120617a632c66f262f3e0e8fb82159743fc1d
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-10 00:36:20 -0500

    set contact form

--- src/components/slides/Contact/index.js
+++ src/components/slides/Contact/index.js
@@ -8,6 +8,10 @@ function Contact() {
   const [message, setMessageValue] = useState('');
   const [status, setStatusValue] = useState('');
 
+  const isValid = () => {
+    return name && email && subject && message ? true : false;
+  }
+
   const resetValues = () => {
     setNameValue('');
     setEmailValue('');
@@ -16,7 +20,12 @@ function Contact() {
   };
 
   const sendMessage = () => {
-    setStatusValue(`Thank you ${name}, expect a response from us soon!`);
+    if (isValid()) {
+      setStatusValue(`Thank you ${name}, expect a response from us soon!`);
+    } else {
+      setStatusValue('Please make sure all fields are filled.');
+    }
+
     resetValues();
     setTimeout(() => {
       setStatusValue('')
@@ -27,14 +36,15 @@ function Contact() {
     <section className='page_section contact'>
       <section className='contact_form'>
         <h1>Contact Us</h1>
+        {status && <h2>{status}</h2>}
         <section className='form'>
           <section className='inputs'>
-            <input type='text' placeholder='Name' name='name' autoComplete='off' onChange={e => setNameValue(e.target.value)} />
-            <input type='email' placeholder='Email' name='email' autoComplete='off' onChange={e => setEmailValue(e.target.value)} />
-            <input type='text' placeholder='Subject' name='subject' autoComplete='off' onChange={e => setSubjectValue(e.target.value)} />
+            <input type='text' placeholder='Name' name='name' autoComplete='off' onChange={e => setNameValue(e.target.value)} required />
+            <input type='email' placeholder='Email' name='email' autoComplete='off' onChange={e => setEmailValue(e.target.value)} required />
+            <input type='text' placeholder='Subject' name='subject' autoComplete='off' onChange={e => setSubjectValue(e.target.value)} required />
           </section>
-          <textarea name='message' placeholder="Tell us what's on your mind" required onChange={e => setMessageValue(e.target.value)} ></textarea>
-          <section className='form_button'><p>Send</p></section>
+          <textarea name='message' placeholder="Tell us what's on your mind" onChange={e => setMessageValue(e.target.value)} ></textarea>
+          <section className='form_button' onClick={() => sendMessage()}><p>Send</p></section>
         </section>
       </section>
     </section>
