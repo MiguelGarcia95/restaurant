a5d648c087948562494ebd7308c858fd655459c3
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-10 16:36:04 -0500

    make app responsive

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -5,9 +5,11 @@ function Navbar({setCurrentSlide, currentSlide}) {
   const activeSlide = slide => slide === currentSlide ? true : false; 
   return (
     <section className='navbar'>
-      <span className={activeSlide('welcome') ? 'active' : ''} onClick={() => setCurrentSlide('welcome')} ><p>Welcome</p></span>
-      <span className={activeSlide('menu') ? 'active' : ''} onClick={() => setCurrentSlide('menu')} ><p>Menu</p></span>
-      <span className={activeSlide('contact') ? 'active' : ''} onClick={() => setCurrentSlide('contact')} ><p>Contact</p></span>
+      <section className='direction'>
+        <span className={activeSlide('welcome') ? 'active' : ''} onClick={() => setCurrentSlide('welcome')} ><p>Welcome</p></span>
+        <span className={activeSlide('menu') ? 'active' : ''} onClick={() => setCurrentSlide('menu')} ><p>Menu</p></span>
+        <span className={activeSlide('contact') ? 'active' : ''} onClick={() => setCurrentSlide('contact')} ><p>Contact</p></span>
+      </section>
     </section>
   );
 }
