8e28b9ff54855a5de923350ba50c5e0bcca34b7c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-10 14:36:55 -0500

    make app responsive

--- src/components/slides/Welcome/index.js
+++ src/components/slides/Welcome/index.js
@@ -6,7 +6,7 @@ function Welcome({setCurrentSlide}) {
     <section className='page_section welcome'>
       <section className='content_side'>
         <h1>Hungry?</h1>
-        <h1>Get a bite</h1>
+        <h1>Grab a bite</h1>
         <section className='content-button' onClick={() => setCurrentSlide('menu')}>
           <p>Check our Menu Out</p>
         </section>
