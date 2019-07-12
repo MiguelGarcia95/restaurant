428e9a011ac4d15d3a2d17f7f462f4bef192761e
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-07-12 17:28:24 -0500

    Updates

---
+++ precache-manifest.63c5259553370a1654c82c2ac8ffed5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3068e1a4c0c68ffb537b3cbfe40e40",
+    "url": "miguelgarcia95.github.io/restaurant/index.html"
+  },
+  {
+    "revision": "729a84d972a137ec5876",
+    "url": "miguelgarcia95.github.io/restaurant/static/css/main.7f1a8502.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "miguelgarcia95.github.io/restaurant/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "729a84d972a137ec5876",
+    "url": "miguelgarcia95.github.io/restaurant/static/js/main.eac80137.chunk.js"
+  },
+  {
+    "revision": "e767c3ab62752ede4adf",
+    "url": "miguelgarcia95.github.io/restaurant/static/js/runtime~main.af84b912.js"
+  }
+]);
